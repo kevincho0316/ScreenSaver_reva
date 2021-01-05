e044010b88e307084ed8b6d160dab4757cf8f94d
kevincho <kevincho9029@gmail.com>
2021-01-05 21:52:25 +0900

    first commit

---
+++ ScreenSaver_reva/ScreenSaverForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace ScreenSaver_reva
+{
+    partial class ScreenSaverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::ScreenSaver_reva.Properties.Resources.chungga;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1942, 1102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // ScreenSaverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.BackgroundImage = global::ScreenSaver_reva.Properties.Resources.chunggaback;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1942, 1102);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ScreenSaverForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ScreenSaverForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ScreenSaverForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ScreenSaver_reva/ScreenSaverForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver_reva
+{
+    public partial class ScreenSaverForm : Form
+    {
+        int speed = -5;
+      
+
+        public ScreenSaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Size = new Size(Bounds.Width, Bounds.Height);
+            this.Bounds = Bounds;
+        }
+        /*
+        public pictureBox1(Rectangle Bounds)
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Bounds = Bounds;
+        }
+        */
+
+
+        private Point mouseLocation;
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        private void pictureBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Application.Exit();
+        }
+        
+
+        private void ScreenSaverForm_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+          
+
+            moveTimer.Tick += new EventHandler(moveTimer_Tick);
+            
+            
+
+            moveTimer.Start();
+        }
+
+        private void moveTimer_Tick(object sender, System.EventArgs e) {
+            Random r = new Random();
+            pictureBox1.Location = new Point(pictureBox1.Location.X + speed, pictureBox1.Location.Y);
+            //Console.WriteLine(pictureBox1.Location.X);
+            if (pictureBox1.Location.X < -55)
+            {
+                speed = 5;
+
+                moveTimer.Interval = 5000;
+            }
+            else if (pictureBox1.Location.X > 55)
+            {
+                speed = -5;
+
+                moveTimer.Interval = 5000;
+            }
+            else if (pictureBox1.Location.X == 0)
+            {
+                moveTimer.Interval = 9000;
+            }
+            else
+            {
+                moveTimer.Interval = 30;
+            }
+
+            /*
+             for (int i=1;i<10;i++){
+                 System.Threading.Thread.Sleep(30);
+                 Console.WriteLine("minus");
+             }
+             for (int i = 1; i < 10; i++)
+             {
+                 System.Threading.Thread.Sleep(30);
+                 pictureBox1.Location = new Point(pictureBox1.Location.X + 3, pictureBox1.Location.Y);
+                 Console.WriteLine("plus");
+             }
+             */
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
